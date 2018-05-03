eee4c9597ec751492991046e99d772a00ded6b6e
Albert Marsnik <albert_marsnik@trimble.com>
2018-05-03 18:24:04 +0200

    Target add component

---
+++ fstg/NgWebUi/src/app/baseball/pitching/target-add/target-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-target-add',
+  templateUrl: './target-add.component.html',
+  styleUrls: ['./target-add.component.css']
+})
+export class TargetAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
